f574ae895fb11f656a00c75114cdfe1a3595a78e
Daniel Schulz <faulolio@gmail.com>
2025-05-06 14:20:56 +0200

    fix: credentials

--- .jenkins/Jenkinsfile.groovy
+++ .jenkins/Jenkinsfile.groovy
@@ -6,54 +6,54 @@ pipeline {
 		stage('Index workspace') {
 			steps {
 				script {
-					unityProject {
-						// define unity project location relative to repository
-						LOCATION = ''
-						
-						// If given, automatically use these credentials to license a free Unity version.
-						UNITY_CREDENTIALS : 'Slothsoft-Unity',
-						EMAIL_CREDENTIALS : 'Slothsoft-Google',
+					unityProject (
+							// define unity project location relative to repository
+							LOCATION : '',
 
-						// use auto-versioning based on tags+commits
-						AUTOVERSION = 'git'
+							// If given, automatically use these credentials to license a free Unity version.
+							UNITY_CREDENTIALS : 'Slothsoft-Unity',
+							EMAIL_CREDENTIALS : 'Slothsoft-Google',
 
-						// automatically create C# docs
-						BUILD_DOCUMENTATION = '0'
+							// use auto-versioning based on tags+commits
+							AUTOVERSION : 'git',
 
-						// automatically run Unity Test Runner
-						TEST_UNITY = '0'
+							// automatically create C# docs
+							BUILD_DOCUMENTATION : '0',
 
-						// automatically run dotnet format
-						TEST_FORMATTING = '0'
-						FORMATTING_EXCLUDE = 'Library Assets/Plugins Packages'
+							// automatically run Unity Test Runner
+							TEST_UNITY : '0',
 
-						// which executables to create
-						BUILD_FOR_WINDOWS = '0'
-						BUILD_FOR_LINUX = '0'
-						BUILD_FOR_MAC = '0'
-						BUILD_FOR_WEBGL = '1'
-						BUILD_FOR_ANDROID = '0'
+							// automatically run dotnet format
+							TEST_FORMATTING : '0',
+							FORMATTING_EXCLUDE : 'Library Assets/Plugins Packages',
 
-						// which platforms to deploy to
-						DEPLOY_TO_STEAM = '0'
-						DEPLOY_TO_ITCH = '1'
-						DEPLOY_ON_FAILURE = '1'
+							// which executables to create
+							BUILD_FOR_WINDOWS : '0',
+							BUILD_FOR_LINUX : '0',
+							BUILD_FOR_MAC : '0',
+							BUILD_FOR_WEBGL : '1',
+							BUILD_FOR_ANDROID : '0',
 
-						// configration for deploying to steam
-						STEAM_ID = ''
-						STEAM_DEPOT_WINDOWS = ''
-						STEAM_DEPOT_LINUX = ''
-						STEAM_DEPOT_MAC = ''
-						STEAM_CREDENTIALS = ''
+							// which platforms to deploy to
+							DEPLOY_TO_STEAM : '0',
+							DEPLOY_TO_ITCH : '1',
+							DEPLOY_ON_FAILURE : '1',
 
-						// configuration for deploying to itch
-						ITCH_ID = 'gregorsoenn/retropair'
-						ITCH_CREDENTIALS = 'Faulo-itch.io'
+							// configration for deploying to steam
+							STEAM_ID : '',
+							STEAM_DEPOT_WINDOWS : '',
+							STEAM_DEPOT_LINUX : '',
+							STEAM_DEPOT_MAC : '',
+							STEAM_CREDENTIALS : '',
 
-						// configuration for deploying to Discord
-						REPORT_TO_DISCORD = '0'
-						DISCORD_WEBHOOK = ''
-					}
+							// configuration for deploying to itch
+							ITCH_ID : 'gregorsoenn/retropair',
+							ITCH_CREDENTIALS : 'Faulo-itch.io',
+
+							// configuration for deploying to Discord
+							REPORT_TO_DISCORD : '0',
+							DISCORD_WEBHOOK : '',
+							)
 				}
 			}
 		}
